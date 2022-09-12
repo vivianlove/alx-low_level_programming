ef373aec781ba86a063a45ac88d5f82b3bb3e8df
vivianlove <ezeoruchinenye@gmail.com>
2022-09-12 01:31:24 -0700

    hello print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -print the string in the put function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of are, \n");
+	return (0);
+}
