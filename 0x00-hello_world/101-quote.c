817efa92b7c5c2ccf3cedb6721409b8a3df59cd5
vivianlove <ezeoruchinenye@gmail.com>
2022-09-12 04:19:49 -0700

    prints

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main -print the string in the function
+*
+* Return: 0
+*/
+int main(void)
+{
+	"and that piece of art is useful" -Dora Korpar, 2015-10-19\n",61);
+	return(1)
+}
