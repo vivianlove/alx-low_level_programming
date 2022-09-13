fd86adab4e11afaf5faa4ebb920a0269830394a6
vivianlove <ezeoruchinenye@gmail.com>
2022-09-13 13:59:15 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,9 +12,9 @@ void print_alphabet_x10(void)
 	int j;
 
 	for (j = 0; j < 10; j++)
-	{
-	         for (i = 'a'; i <= 'z'; i++)
-		         _putchar(i);
-		  _putchar('\n');
+	{	
+	       for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
 	}	 
-} 
+}
