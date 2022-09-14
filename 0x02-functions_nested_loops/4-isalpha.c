49d3a86b5097f4933e1daedb946ff33ba35eccba
vivianlove <ezeoruchinenye@gmail.com>
2022-09-14 16:38:58 -0700

    vivian

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
-* _isalpha - tests whether a character is from the English alphabet.
-* @c: character to be checked.
-* Return: 1 if the character is an English character.
-* 0 if the character is not an English character.
-*/
+ * _isalpha - tests whether a character is from the English alphabet.
+ * @c: character to be checked.
+ * Return: 1 if the character is an English character.
+ * 0 if the character is not an English character.
+ */
 int _isalpha(int c)
 {
 	
-	if ((c >= 'a' && c <= 'z') || (c >= _islower ='A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
-		_putchar (0);
+	else
 		return (0);
 }
 
