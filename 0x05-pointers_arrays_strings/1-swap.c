c093b4d901765ade25e66b7f721846728538ae56
vivianlove <ezeoruchinenye@gmail.com>
2022-09-20 20:01:03 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*
+* 
+*
+*
+*
+*/
+
+void swap_int(int *a, int *b);
+{
+	int temp;
+
+	temp = *a;
+	//The value of b is now store inside variable a
+	*a = *b;
+	//Swap the value of a to variable b
+	*b = temp;
+}
