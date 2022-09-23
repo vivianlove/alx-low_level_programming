fd27d60223623715a560fee389c3d979a31b91c8
vivianlove <ezeoruchinenye@gmail.com>
2022-09-23 13:08:10 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * Cap_string - Capitalizes all words of a string.
+ * @str:  The string to be capitalized.
+ *
+ * Return: A pointer to the changed string
+ */
+char *cap_string(char *)
+{
+	int index = 0;
+
+	while (str[index]
+        {
+	      while(!(str[index] >= 'a' && str
+		     index++;
+	       if(str[index - 1] == ' ' ||
+	       if(str[index - 1] == '\t' ||
+	       if(str[index - 1] == '\n' ||
+	       if(str[index - 1] == '.' ||
+	       if(str[index - 1] == ';' ||
+	       if(str[index - 1] == ',' ||
+	       if(str[index - 1] == '|' ||
+	       if(str[index - 1] == '?' ||
+	       if(str[index - 1] == '"' ||
+	       if(str[index - 1] == '(' ||
+	       if(str[index - 1] == ')' ||
+	       if(str[index - 1] == '{' ||
+	       if(str[index - 1] == '}' ||
+	          index == 0)
+                      Str[index] -= 32;
+               index++;		 
+       }
+      return (str);
+}     
