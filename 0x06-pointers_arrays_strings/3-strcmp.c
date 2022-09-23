7bdcff90e7095c3919fcf9d9347243ba44dfa53a
vivianlove <ezeoruchinenye@gmail.com>
2022-09-22 19:57:31 -0700

    4-main.c 4-rev_array.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,9 @@
  * _strcmp - a function that compares two strings.
  * @s1: an input string
  * @s2: an input string
- * Return:
- * 		returns zero if s1 == s2
- * 		returns negative number if s1 < s2
- * 		returns positive number if s1 > s2
+ * Return:  -15 if s1 < s2
+ * 	      0 if s1 = s2
+ * 	      15 if S1 > S2
  */
 
 int _strcmp(char *s1, char *s2)
