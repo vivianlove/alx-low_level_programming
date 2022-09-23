964b64bcc36d82053239a38b707e7fbf5e18578f
vivianlove <ezeoruchinenye@gmail.com>
2022-09-23 00:00:19 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,14 +11,14 @@ char *leet(char *)
 	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
         while (str[indx1])
-	}
-	       for (indx2 == 0; indx2 <=7; indx2++)
+        {
+	       for (indx2 = 0; indx2 <=7; indx2++)
 	       { 
-	             if (str[indx1] == leet[indx2++) ||
-	                 str[indx1] -32 == leet[indx2]
+	             if (str[indx1] == leet[indx2) ||
+	                 str[indx1] -32 == leet[indx2])
 	                     str[indx1] = indx2 + '0';
 		}
-	        indux1++;
+	        indx1++;
        {	
        return (str);
 }       
