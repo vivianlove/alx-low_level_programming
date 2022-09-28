537a8f3e0d6ef2d4d008e69ebf880afd461418b0
vivianlove <ezeoruchinenye@gmail.com>
2022-09-27 17:36:24 -0700

    commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@
 
 #include <stddef.h>
 
-int _putchar(char c)i;
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -19,5 +19,3 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-
-#endif /*MAIN_H*/ 
