ff5432d31a82e5afd4c5841d548cbee30d060b88
vivianlove <ezeoruchinenye@gmail.com>
2022-09-29 17:46:38 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,13 @@
  * _puts_recursion - prints a string, followed by a new line
  * @c: The string to be printed
  */
-void _puts_recursion(char *s);
- { 
+void _puts_recursion(char *s)
+{
 	if (*s)
 	{
 		_putchar(*s);
-		_puts-recurssions(s + 1)
+		_puts - recurssions(s + 1);
 	}
 	else
 		putchar('\n');
- }			
+}
