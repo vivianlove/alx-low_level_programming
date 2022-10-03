e213445355405836503c2ac96fe04420cbb90b24
vivianlove <ezeoruchinenye@gmail.com>
2022-10-03 06:48:10 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,10 @@
 /**
  * main - prints the name of the program
  * @rgc: number of arguments passed to the function
- * @argv: argument vectir of pointers to string
+ * @argv: argument vector of pointers to string
  * Return: always 0
 int main(int argc _attribute_((unused)), char *argv[])
 {
-	   printf("%\n", argvargv[0]);
-	return (0);
+	   printf("%\n", argv[0]);
+	   return (0);
 }
