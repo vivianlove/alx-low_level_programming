464a0c7c1349d57dea89738dc055e3e566f09cce
vivianlove <ezeoruchinenye@gmail.com>
2022-10-24 04:48:03 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIABLE FUNCTION_H
+#define VARIABLE FUNCTION_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
