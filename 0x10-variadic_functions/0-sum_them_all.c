3559d3efee026dc1d96e615ff93a25e28969b7d4
vivianlove <ezeoruchinenye@gmail.com>
2022-11-02 11:36:48 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,19 +1,28 @@
-#include <stdio.h>
 #include "variadic_functions.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * sum_them_all - sum of variadic variable
+ * @n:const
+ * Return:int
  */
-int main(void)
+int sum_them_all(const unsigned int n, ...)
 {
+unsigned int i;
+va_list list;
+double sum = 0;
 
-	int sum;
-
-	sum = sum_them_all(2, 98, 1024);
-	printf("%d\n", sum);
-	sum = sum_them_all(4, 98, 1024, 402, -1024);
-	printf("%d\n", sum);
-	return (0);
+if (n == 0)
+{
+return (0);
+}
+if (n == 0)
+{
+return (0);
+}
+va_start(list, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(list, int);
+}
+va_end(list);
+return (sum);
 }
